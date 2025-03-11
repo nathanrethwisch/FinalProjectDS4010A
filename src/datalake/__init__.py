05ad8629cdcc465afbc9030ad02c3b8ed4cba622
Dhruv Dole <dhruv.dole@sdole.com>
2025-03-11 16:12:54 -0500

    working

--- src/datalake/__init__.py
+++ src/datalake/__init__.py
@@ -1,20 +1,16 @@
 import json
 import os
+import tempfile
+import zipfile
 from io import StringIO
 from pathlib import Path
-from shapely.geometry import shape, Polygon, mapping
+from shapely.geometry import shape, Polygon, mapping, Point
 from shapely.validation import make_valid
 import geopandas as gpd
 import pandas as pd
 import pyarrow.dataset as ds
+import pyarrow.parquet as pq
 import requests
-
-paths = {"ghcnd_raw_stations": "raw/ghcnd/stations.txt", "ghcnd_raw_daily": "raw/ghcnd/daily/",
-         "ghcnd_clean_stations": "clean/ghcnd/stations.parquet", "ghcnd_metadata": "clean/ghcnd/metadata.html",
-         "ghcnd_clean_daily": "clean/ghcnd/daily/",
-         "fire_point_raw": "raw/fire_occurrence_point/National_USFS_Fire_Occurrence_Point_(Feature_Layer).geojson",
-         "fire_perimeter_raw": "raw/fire_perimeter/National_USFS_Fire_Perimeter_(Feature_Layer).geojson",
-         "fire_perimeter_cleaned" : "clean/fire_perimeter/National_USFS_Fire_Perimeter_(Feature_Layer).parquet",}
 
 ghcnd_sources = {"readme": "https://docs.opendata.aws/noaa-ghcn-pds/readme.html",
                  "stations": "https://noaa-ghcn-pds.s3.amazonaws.com/ghcnd-stations.txt",
@@ -22,31 +18,24 @@ ghcnd_sources = {"readme": "https://docs.opendata.aws/noaa-ghcn-pds/readme.html"
                  "bucket": "https://noaa-ghcn-pds.s3.amazonaws.com"}
 years = (2000, 2025)
 elements = ['PRCP', 'SNOW', 'SNWD', 'TMAX', 'TMIN', 'AWND']
-datalake_root = Path('../lake')
 
 
-def read_geojson(path):
-    print("READING/REPAIRING GEOJSON ")
-    with open(path, 'r') as f:
-        data = json.load(f)
-    for feature in data['features']:
-        try:
-            geometry = shape(feature['geometry'])
-            if not geometry.is_valid:
-                geometry = make_valid(geometry)
-            feature['geometry'] = mapping(geometry)
-        except Exception as e:
-            print(f"Error processing geometry: {e}")
-            feature['geometry'] = mapping(Polygon())  # Replace with an empty polygon
-    geojson_str = json.dumps(data)
-    geojson_file = StringIO(geojson_str)
-    gdf = gpd.read_file(geojson_file).to_crs(epsg=4326)
-    return gdf
-
+# datalake_root = Path('../lake')
 
 class Datalake:
-    def __init__(self):
-        pass  # self.datalake_root = Path(input("where?"))
+
+    def __init__(self, root_path):
+        self.datalake_root = Path(root_path)
+        self.paths = {"ghcnd_raw_stations": "raw/ghcnd/stations.txt", "ghcnd_raw_daily": "raw/ghcnd/daily/",
+                      "ghcnd_clean_stations": "clean/ghcnd/stations.parquet",
+                      "ghcnd_metadata": "clean/ghcnd/metadata.html",
+                      "ghcnd_clean_daily": "clean/ghcnd/daily/",
+                      "fire_point_raw": "raw/fire_occurrence_point/National_USFS_Fire_Occurrence_Point_(Feature_Layer).geojson",
+                      "fire_point_clean": "clean/fire_occurrence_point/National_USFS_Fire_Occurrence_Point_(Feature_Layer).parquet",
+                      "fire_perimeter_raw": "raw/fire_perimeter/National_USFS_Fire_Perimeter_(Feature_Layer).geojson",
+                      "fire_perimeter_cleaned": "clean/fire_perimeter/National_USFS_Fire_Perimeter_(Feature_Layer).parquet",
+                      "states_raw": "raw/maps/cb_2023_us_all_500k.zip",
+                      "states_clean": "clean/maps/cb_2023_us_states_500k.parquet", }
 
     def initialize(self):
         self._create_subdirectory('raw')
@@ -83,13 +72,12 @@ class Datalake:
                     file.write(chunk)
         pass
 
-    @staticmethod
-    def _create_subdirectory(relative_path):
+    def _create_subdirectory(self, relative_path):
         """
         Creates a directory using paths relative to the datalake root
         :param relative_path:
         """
-        path = datalake_root / relative_path
+        path = self.datalake_root / relative_path
         if not os.path.exists(path):
             os.makedirs(path)
             print(f'{relative_path} folder created')
@@ -100,58 +88,160 @@ class Datalake:
         pass
 
     def download_ghcnd(self):
-        self._download_file(ghcnd_sources["readme"], datalake_root / paths["ghcnd_metadata"])
+        self._download_file(ghcnd_sources["readme"], self.datalake_root / self.paths["ghcnd_metadata"])
         # download stations
-        self._download_file(ghcnd_sources['stations'], datalake_root / paths["ghcnd_raw_stations"])
+        self._download_file(ghcnd_sources['stations'], self.datalake_root / self.paths["ghcnd_raw_stations"])
         # download daily
         for year in range(years[0], years[1] + 1):
             file_url = f'{ghcnd_sources["bucket"]}/csv.gz/by_year/{year}.csv.gz'
-            dest_path = datalake_root / paths['ghcnd_raw_daily'] / f'{year}.csv.gz'
+            dest_path = self.datalake_root / self.paths['ghcnd_raw_daily'] / f'{year}.csv.gz'
             self._download_file(file_url, dest_path)
 
-    @staticmethod
-    def clean_ghcnd_stations():
+    def clean_ghcnd_stations(self):
         print('CLEANING GHCND STATIONS')
-        clean_stations_file(datalake_root / paths["ghcnd_raw_stations"], datalake_root / paths["ghcnd_clean_stations"])
+        clean_stations_file(self.datalake_root / self.paths["ghcnd_raw_stations"],
+                            self.datalake_root / self.paths["ghcnd_clean_stations"])
 
-    @staticmethod
-    def clean_daily():
+    def clean_daily(self):
         for year in range(years[0], years[1] + 1):
             print(f'CLEANING DAILY: {year}')
-            clean_daily_file(datalake_root / paths["ghcnd_raw_daily"] / f'{year}.csv.gz',
-                             datalake_root / paths["ghcnd_clean_daily"] / f'{year}.parquet')
+            clean_daily_file(self.datalake_root / self.paths["ghcnd_raw_daily"] / f'{year}.csv.gz',
+                             self.datalake_root / self.paths["ghcnd_clean_daily"] / f'{year}.parquet')
 
-    @staticmethod
-    def query_ghcnd(query_expression, columns=None):
+    def query_ghcnd(self, pyarrow_query, columns=None):
         if columns and 'geometry' not in columns:
             return ValueError("Columns list is provided but does not contain 'geometry'!")
         else:
-            dataset = ds.dataset(datalake_root / paths["ghcnd_clean_daily"])
-            return gpd.GeoDataFrame.from_arrow(dataset.filter(query_expression).to_table(columns=columns)).set_crs(
+            dataset = ds.dataset(self.datalake_root / self.paths["ghcnd_clean_daily"])
+            return gpd.GeoDataFrame.from_arrow(dataset.filter(pyarrow_query).to_table(columns=columns)).set_crs(
                 epsg=4326)
 
-    @staticmethod
-    def query_fire_point(query_expression=None, columns=None):
-        return gpd.read_file(datalake_root / paths['fire_point_raw']).to_crs(
-            epsg=4326)  # TODO, wrap the r script and return the cleaned version
+    def clean_states(self):
+        raw_path = self.datalake_root / self.paths["states_raw"]
+        clean_path = self.datalake_root / self.paths["states_clean"]
+        states = None
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with zipfile.ZipFile(raw_path, 'r') as zip_ref:
+                zip_ref.extractall(temp_dir)
+            files = os.listdir(temp_dir)
+            states = gpd.read_file(os.path.join(temp_dir, files[0]), layer='cb_2023_us_state_500k')
+        not_conus = ['District of Columbia', 'American Samoa', 'United States Virgin Islands',
+                     'Commonwealth of the Northern Mariana Islands', 'Puerto Rico', 'Guam', 'Hawaii', 'Alaska']
+        states = states[~states['NAME'].isin(not_conus)]
+        states = states[['NAME', "geometry"]]
+        states.columns = ['name', 'geometry']
+        states = states.to_crs(epsg=4326)
+        states.to_parquet(clean_path)
 
-    @staticmethod
-    def query_fire_perimeter(query_expression=None, columns=None):
+    def query_states(self):
+        return gpd.read_parquet(self.datalake_root / self.paths["states_clean"])
+
+    def _repair_geojson(self, path):
+        print("READING/REPAIRING GEOJSON ")
+        with open(path, 'r') as f:
+            data = json.load(f)
+        for feature in data['features']:
+            try:
+                geometry = shape(feature['geometry'])
+                if not geometry.is_valid:
+                    geometry = make_valid(geometry)
+                feature['geometry'] = mapping(geometry)
+            except Exception as e:
+                print(f"Error processing geometry: {e}")
+                feature['geometry'] = mapping(Polygon())  # Replace with an empty polygon
+        geojson_str = json.dumps(data)
+        geojson_file = StringIO(geojson_str)
+        gdf = gpd.read_file(geojson_file).to_crs(epsg=4326)
+        return gdf
+
+    def process_fire_perimeter(self):
+        gdf = self._repair_geojson(self.datalake_root / self.paths["fire_perimeter_raw"])
+        gdf = self.drop_not_conus(gdf)
+        gdf = self._clean_fire_perimeter(gdf)
+        gdf.to_parquet(self.datalake_root / self.paths["fire_perimeter_cleaned"])
+
+    def drop_not_conus(self, gdf):
+        states = self.query_states()
+        union = states.geometry.union_all()
+        gdf = gdf[gdf.geometry.within(union)]
+        return gdf
+
+    def _clean_fire_perimeter(self, gdf):
+        gdf = gdf.to_crs(epsg=4326)
+        # TODO DROP UNWANTED COLS
+        # drop = [
+        #     'SHAPE', 'GLOBALID', 'REVDATE',
+        #     'COMPLEXNAME', 'SOFIRENUM', 'LOCALFIRENUM',
+        #     'SECURITYID', 'DATASOURCE', 'OWNERAGENCY',
+        #     'UNITIDOWNER', 'PROTECTIONAGENCY', 'UNITIDPROTECT',
+        #     'POINTTYPE', 'FIRERPTQC', 'DBSOURCEID',
+        #     'DBSOURCEDATE', 'ACCURACY', 'SHAPE'
+        # ]
+        # gdf.drop(columns=drop, inplace=True)
+        # TODO CREATE YEAR MONTH DAY COLS
+        # TODO RENAME COLUMN NAMES
+        return gdf
+
+    def query_fire_perimeter(self, pyarrow_query=None, columns=None):
         if columns and 'geometry' not in columns:
             return ValueError("Columns list is provided but does not contain 'geometry'!")
+        elif pyarrow_query:
+            table = pq.read_table(self.datalake_root / self.paths["fire_perimeter_cleaned"], columns=columns).filter(
+                pyarrow_query)
+            return gpd.GeoDataFrame.from_arrow(table).to_crs(epsg=4326)
         else:
-            return gpd.read_parquet(datalake_root / paths['fire_perimeter_cleaned'], columns=columns)
+            table = pq.read_table(self.datalake_root / self.paths["fire_perimeter_cleaned"], columns=columns)
+            return gpd.GeoDataFrame.from_arrow(table).to_crs(epsg=4326)
 
-    @staticmethod
-    def get_us_states():
-        return gpd.read_file(datalake_root / "clean/maps/cb_2023_us_all_500k.gdb",
-                             layer='cb_2023_us_state_500k').to_crs(epsg=4326)
+    def process_fire_point(self):
+        gdf = gpd.read_file(self.datalake_root / self.paths["fire_point_raw"])
+        # gdf = self.drop_not_conus(gdf) # TODO THIS TAKES YEARS
+        gdf = self._clean_fire_point(gdf)
+        gdf.to_parquet(self.datalake_root / self.paths["fire_point_clean"])
 
-    @staticmethod
-    def clean_fire_perimeter():
-        gdf = read_geojson(datalake_root / paths["fire_perimeter_raw"])
+    def _clean_fire_point(self, gdf):
+        gdf.to_crs(epsg=4326)
+        # TODO DROP UNWANTED COLS
+        drop = [
+            'SHAPE', 'GLOBALID', 'REVDATE',
+            'COMPLEXNAME', 'SOFIRENUM', 'LOCALFIRENUM',
+            'SECURITYID', 'DATASOURCE', 'OWNERAGENCY',
+            'UNITIDOWNER', 'PROTECTIONAGENCY', 'UNITIDPROTECT',
+            'POINTTYPE', 'FIRERPTQC', 'DBSOURCEID',
+            'DBSOURCEDATE', 'ACCURACY', 'SHAPE',
+            'FIREOUTDATETIME', 'FIREYEAR'
+        ]
+        gdf = gdf.drop(columns=drop)
 
-        gdf.to_parquet(datalake_root / paths["fire_perimeter_cleaned"])
+        # TODO PROCESS DATES
+        gdf = gdf.dropna(subset=['DISCOVERYDATETIME'])
+        dates = gdf['DISCOVERYDATETIME'].str.split(" ", expand=True)[0].str.split('/', expand=True)
+        dates.columns = ['year', 'month', 'day']
+        gdf = pd.concat([gdf, dates], axis=1)
+        gdf = gdf.drop(columns=['DISCOVERYDATETIME'])
+
+        # TODO REPAIR POINTS
+        gdf['geometry'] = gdf.apply(
+            lambda row: Point(row['LONGDD83'], row['LATDD83']) if pd.isnull(row['geometry']) and pd.notnull(
+                row['LATDD83']) and pd.notnull(row['LONGDD83']) else row['geometry'],
+            axis=1
+        )
+        gdf = gdf.drop(columns=['LATDD83', 'LONGDD83'])
+
+        # TODO RENAME COLUMN NAMES
+
+        return gdf
+
+    def query_fire_point(self, pyarrow_query=None, columns=None):
+        if columns and 'geometry' not in columns:
+            return ValueError("Columns list is provided but does not contain 'geometry'!")
+        elif pyarrow_query:
+            table = pq.read_table(self.datalake_root / self.paths["fire_point_clean"], columns=columns).filter(
+                pyarrow_query)
+            return gpd.GeoDataFrame.from_arrow(table).to_crs(epsg=4326)
+        else:
+            table = pq.read_table(self.datalake_root / self.paths["fire_point_clean"], columns=columns)
+            return gpd.GeoDataFrame.from_arrow(table).to_crs(epsg=4326)
 
 
 def clean_stations_file(raw_path, clean_path):
@@ -178,8 +268,8 @@ def clean_stations_file(raw_path, clean_path):
     stations.to_parquet(clean_path)
 
 
-def get_stations():
-    stations = pd.read_parquet(datalake_root / paths["ghcnd_clean_stations"])
+def get_stations(self):
+    stations = pd.read_parquet(self.datalake_root / self.paths["ghcnd_clean_stations"])
     us_stations = stations[stations['station_id'].str.startswith('US')]
     ca_stations = stations[stations['station_id'].str.startswith('CA')]
     mx_stations = stations[stations['station_id'].str.startswith('MX')]
--- src/fire_perimeter.py
+++ src/fire_perimeter.py
@@ -2,5 +2,4 @@ from datalake import Datalake
 
 if __name__ == '__main__':
     lake = Datalake()
-    lake.initialize()
-    lake.clean_fire_perimeter()
+    lake.process_fire_perimeter()
