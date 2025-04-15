6be03bfa348c04d1aac12610181d4f34a5c80a74
nathanrethwisch <nreth@iastate.edu>
2025-04-15 10:04:24 -0500

    Adding code to main.py that allows for cliking on a point and seeing the model output table

--- src/main.py
+++ src/main.py
@@ -19,9 +19,13 @@ from app.utils import *
 import geopandas as gpd
 
 # Initialization
+
+
 ASSETS_ROOT = Path(os.getenv('ASSETS_ROOT'))
-CELL_RESOLUTION = None
+CELL_RESOLUTION = 3
 data = gpd.read_parquet(ASSETS_ROOT / 'model_output.parquet')
+
+
 # hex_ids = data['Hexagon_ID'].unique().tolist()  # TODO DON'T THINK THIS IS NEEDED ANYMORE
 
 app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY],
@@ -59,9 +63,9 @@ def render_tab_content(tab):
                         dl.TileLayer(),
                         dl.LayersControl([], id="lc", collapsed=False, position="bottomright")
                     ], center=[40, -95], zoom=4, style={'height': '50vh'}, id="map"),
-                    html.Div(id="colorbar", style={"height": "30px", "margin": "10px 0px"}),
+                    html.Div(id="colorbar", style={"height": "30px", "margin": "20px 20px"}),
                     html.Button("Recenter", id="recenter"),
-                ], style={'width': '60%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding': '10px'}),
+                ], style={'width': '75%', 'display': 'inline-block', 'verticalAlign': 'top', 'padding': '20px'}),
                 html.Div([
                     html.H3("Model"),
                     html.Div(id='output-container'),
@@ -85,7 +89,7 @@ def render_tab_content(tab):
         return html.Div([
             html.H3("Fire Occurrence Over Time"),
             html.Iframe(
-                src="/assets/fire_timeseries.html",  # TODO MOVE THIS TO ENV ROOT
+                src="/fire_timeseries.html",  # TODO MOVE THIS TO ENV ROOT
                 style={"width": "100%", "height": "600px", "border": "none"}
             )
         ])
@@ -94,7 +98,7 @@ def render_tab_content(tab):
         return html.Div([
             html.H3("Dashboard Information"),
             html.Iframe(
-                src="/assets/model-info.html",  # TODO MOVE THIS TO ENV ROOT
+                src= "/assets/model-info.html",  # TODO MOVE THIS TO ENV ROOT
                 style={"width": "100%", "height": "600px", "border": "none"}
             )
         ])
@@ -131,17 +135,63 @@ def update_map(date_str: str):
     return generate_layers(subset)
 
 
-@app.callback(Output('output-container', 'children'),
-              Input('map', 'clickData'),
-              )
-def show_click_data(clickData):
-    result = f"""
-    map: {json.dumps(clickData)}
-    """
+@app.callback(
+    Output('output-container', 'children'),
+    Input('map', 'clickData'),
+    Input('date-picker', 'date'),
+)
+def show_click_data(clickData, date_str):
+    if not clickData or not date_str:
+        return f"Click on a hexagon to view model predictions for {date_str}."
+
     lat = clickData['latlng']['lat']
     lon = clickData['latlng']['lng']
     cell = latlng_to_cell(lat, lon, CELL_RESOLUTION)
-    return result
+
+    year, month, day = map(int, date_str.split('-'))
+    filtered = data[
+        (data['year'] == year) &
+        (data['month'] == month) &
+        (data['day'] == day) &
+        (data['Hexagon_ID'] == cell)
+    ]
+    
+    columns_to_keep = [
+            'Predicted Fire Probability',
+            'Temperature Maximum (3-Day Average)',
+            'Temperature Minimum (3-Day Average)',
+            'Precipitation (3-Day Average)',
+            'Snowfall (3-Day Average)',
+            'Daily Average Wind (3-Day Average)',
+            'Average Elevation',
+            'Fire Occurred?',            
+            'geometry'
+        ]
+        
+    filtered = filtered[columns_to_keep]  
+    filtered['Fire Occurred?'] = filtered['Fire Occurred?'].map({0: "No", 1: "Yes"})
+    filtered['Temperature Maximum (3-Day Average)'] = filtered['Temperature Maximum (3-Day Average)'].apply(lambda s: f"{s * 0.18 + 32:.2f}°F")
+    filtered['Temperature Minimum (3-Day Average)'] = filtered['Temperature Minimum (3-Day Average)'].apply(lambda s: f"{s * 0.18 + 32:.2f}°F")
+    filtered['Snowfall (3-Day Average)'] = filtered['Snowfall (3-Day Average)'].apply(lambda s: f"{s / 10:.4f}mm")
+    filtered['Precipitation (3-Day Average)'] = filtered['Precipitation (3-Day Average)'].apply(lambda s: f"{s / 10:.4f}mm")
+    filtered['Predicted Fire Probability'] = filtered['Predicted Fire Probability'].apply(lambda s: f"{s:.5f}")
+    
+    
+    filtered = filtered.round(5)
+    
+        
+    transposed = filtered.drop(columns='geometry').T
+    transposed.columns = ['Value']
+    transposed.reset_index(inplace=True)
+    transposed.columns = ['Variable', 'Value']
+    
+    table = dbc.Table.from_dataframe(transposed, striped=True, bordered=True, hover=True)    
+    #table = dbc.Table.from_dataframe(filtered.drop(columns='geometry'), striped=True, bordered=True, hover=True)
+
+    return html.Div([
+        html.Div(table, style={"font-size": "12px"})
+    ])
+
 
 
 @app.callback(
